dfb88de42859724a841a0301ce8644db9204f891
mino-park7 <minho.park2115@gmail.com>
2017-12-11 11:03:31 +0900

    train file modify

--- im2txt/train.sh
+++ im2txt/train.sh
@@ -1,11 +1,11 @@
-MSCOCO_DIR="/tmp/mscoco"
-HOME="/home/minhopark2115/git-reposits/ShowAndTell/ShowAndTell-practice"
-INCEPTION_CHECKPOINT="${HOME}/im2txt/data/inception_v3.ckpt"
-MODEL_DIR="${HOME}/im2txt/model"
-
-python3 train.py\
-    --input_file_pattern="${MSCOCO_DIR}/train-?????-of-00256"\
-    --inception-checkpoint-file="${INCEPTION_CHECKPOINT}"\
+MSCOCO_DIR="${HOME}/git-reposits/ShowAndTell/ShowAndTell-practice/im2txt/data/mscoco"
+INCEPTION_CHECKPOINT="${HOME}/git-reposits/ShowAndTell/ShowAndTell-practice/im2txt/data/inception_v3.ckpt"
+MODEL_DIR="${HOME}/git-reposits/ShowAndTell/ShowAndTell-practice/im2txt/model"
+echo ${INCEPTION_CHECKPOINT}
+echo ${MODEL_DIR}
+python3 train.py \
+    --input_file_pattern="${MSCOCO_DIR}/train-?????-of-00256" \
+    --inception_checkpoint_file="${INCEPTION_CHECKPOINT}"\
     --train_dir="${MODEL_DIR}/train" \
     --train_inception=false \
     --number_of_steps=1000000
